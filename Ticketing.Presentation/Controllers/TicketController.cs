3e7ceebae0c970125850d5b8eb073d807c812425
negin <negin1380farahani@gmail.com>
2025-12-25 22:15:18 +0330

    feat(add): Add TicketController and implement endpoints: CreateTicket (create new ticket), DeleteTicket (delete ticket by ID), GetTickets (fetch all tickets), and GetTicketDetails (fetch ticket details with chats)

--- Ticketing.Presentation/Controllers/TicketController.cs
+++ Ticketing.Presentation/Controllers/TicketController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ticketing.Application.DTO;
+using Ticketing.Application.Interfaces.Services;
+using Ticketing.infra.Services;
 
 namespace Ticketing.Presentation.Controllers
 {
@@ -7,24 +10,46 @@ namespace Ticketing.Presentation.Controllers
     [ApiController]
     public class TicketController : ControllerBase
     {
+        private readonly TicketService _ticketService;
+        public TicketController(TicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> CreateTicket(int id)
+        {
+            await _ticketService.DeleteById(id);
+            return NoContent();
+        }
+
+
         [HttpPost]
-        public IActionResult CreateTicket()
+        public async Task<IActionResult> CreateTicket(CreateTicketDto createTicketDto)
         {
-
+            await _ticketService.AddAsync(createTicketDto);
+            return NoContent();
         }
 
-        [HttpGet]
-        public IActionResult GetAllTickets()
-        {
-
-        }
 
 
         [HttpGet]
-        public IActionResult GetTicketById()
+        public async Task<IActionResult> GetTickets()
         {
-            var ticket =
+            var tickets = await _ticketService.GetAll();
+            return Ok(tickets);
+
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTicketDetaild(int id)
+        {
+            var ticketdetaildto = await _ticketService.GetByIdAsync(id);
+            return Ok(ticketdetaildto);
+
+
+        }
+
 
     }
 }
