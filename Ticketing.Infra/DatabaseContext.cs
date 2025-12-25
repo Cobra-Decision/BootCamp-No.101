e5fc4ea7a0aaa2352468bd66c0023995211c0703
negin <negin1380farahani@gmail.com>
2025-12-25 19:20:22 +0330

    feat(relation dbcontext) اضافه کردن fluent api ها به دیبی Databasecontext

--- Ticketing.Infra/DatabaseContext.cs
+++ Ticketing.Infra/DatabaseContext.cs
@@ -21,5 +21,35 @@ namespace Ticketing.infra
 
         }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ticket>()
+                 .HasOne(t => t.User)
+                 .WithMany(u => u.Tickets)
+                 .HasForeignKey(t => t.UserId)
+                 .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<TicketMessage>()
+                 .HasOne(tm => tm.User)
+                 .WithMany(u => u.TicketMessages)
+                 .HasForeignKey(tm => tm.UserId)
+                 .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<TicketMessage>()
+                .HasOne(tm => tm.Ticket)
+                .WithMany(t => t.TicketMessages)
+                .HasForeignKey(tm => tm.TicketId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Ticket>()
+                .HasOne(t => t.TicketStatus)
+                .WithMany(ts => ts.Tickets)
+                .HasForeignKey(t => t.TicketStatusId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
