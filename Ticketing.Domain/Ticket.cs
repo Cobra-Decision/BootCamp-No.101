79ff0d1509448b1daf49ae5b9691283d3b2c1ada
MohsenBg <moh.1380.1393@gmail.com>
2025-12-26 19:45:26 +0330

    feat(Domain/Ticket): add required modifiers and UTC defaults to Ticket

--- Ticketing.Domain/Ticket.cs
+++ Ticketing.Domain/Ticket.cs
@@ -1,29 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ticketing.Domain
+﻿namespace Ticketing.Domain
 {
     public class Ticket
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public int Department_id { get; set; }        
-        public DateTime Created_at { get; set; }
-        public DateTime Updated_at { get; set; }
 
+        public required string Title { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
         public int TicketStatusId { get; set; }
-        public TicketStatus TicketStatus { get; set; }
+        public TicketStatus TicketStatus { get; set; } = null!;
 
         public int UserId { get; set; }
-        public User User { get; set; }
+        public User User { get; set; } = null!;
 
-        public List<TicketMessage> TicketMessages { get; set; }
-
-
+        public List<TicketMessage> TicketMessages { get; set; } = new();
     }
 }
