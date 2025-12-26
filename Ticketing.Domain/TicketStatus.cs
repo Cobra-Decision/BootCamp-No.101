6b7d810bf7b3a2a5b09084b82d342fe46db7d442
MohsenBg <moh.1380.1393@gmail.com>
2025-12-26 19:47:07 +0330

    feat(Domain/TicketStatus): modernize TicketStatus and initialize collections

--- Ticketing.Domain/TicketStatus.cs
+++ Ticketing.Domain/TicketStatus.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ticketing.Domain
+﻿namespace Ticketing.Domain
 {
     public class TicketStatus
     {
         public int Id { get; set; }
-        public string Title { get; set; }
 
+        public required string Title { get; set; }
 
-        
-        public List<Ticket> Tickets { get; set; }
-
-       
-      
+        // --- Relations ---
+        public List<Ticket> Tickets { get; set; } = new();
     }
 }
