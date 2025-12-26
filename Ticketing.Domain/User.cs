4d4dd2d4de7352664f0e22c429fec1d05398ae4c
MohsenBg <moh.1380.1393@gmail.com>
2025-12-26 19:55:52 +0330

    feat(Domain/User): modernize User entity with required properties and PascalCase naming

--- Ticketing.Domain/User.cs
+++ Ticketing.Domain/User.cs
@@ -1,29 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Ticketing.Domain
 {
     public class User
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Phone { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public bool IsActive { get; set; }
-        public DateTime createAt { get; set; }
 
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
 
-       
-        public List<Ticket> Tickets {get; set;}
-        public List<TicketMessage> TicketMessages { get; set; }
+        // These are required for your LoginAsync logic (Phone or Email)
+        public required string Phone { get; set; }
+        public required string Email { get; set; }
 
-        
+        public required string Password { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        // Renamed to PascalCase and added default value
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // --- Relations ---
+        public List<Ticket> Tickets { get; set; } = new();
+        public List<TicketMessage> TicketMessages { get; set; } = new();
     }
 }
