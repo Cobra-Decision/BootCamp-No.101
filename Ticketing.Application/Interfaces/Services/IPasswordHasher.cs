c1543279f0b3d0f7356d9ebe473797459bc42c28
MohsenBg <moh.1380.1393@gmail.com>
2025-12-26 20:01:11 +0330

    feat(service/PasswordHasher): define IPasswordHasher for secure credential handling

---
+++ Ticketing.Application/Interfaces/Services/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+
+namespace Ticketing.Application.Interfaces.Services
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string hashedPassword);
+    }
+}
