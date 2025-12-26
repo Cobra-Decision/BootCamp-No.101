f2459e86debcb80b187e93b94f3a44071ca2e767
MohsenBg <moh.1380.1393@gmail.com>
2025-12-26 19:45:47 +0330

    feat(Domain/TicketMessage): update TicketMessage with required fields and relations

--- Ticketing.Domain/TicketMessage.cs
+++ Ticketing.Domain/TicketMessage.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ticketing.Domain
+﻿namespace Ticketing.Domain
 {
     public class TicketMessage
     {
         public int Id { get; set; }
-        public string Message { get; set; }
-        public DateTime Created_At { get; set; }
 
+        public required string Message { get; set; }
 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // --- Relations ---
+        // Foreign Key for Ticket
         public int TicketId { get; set; }
-        public Ticket Ticket { get; set; }
+        public Ticket Ticket { get; set; } = null!;
 
+        // Foreign Key for User (The sender of the message)
         public int UserId { get; set; }
-        public User User { get; set; }
-
-
-
+        public User User { get; set; } = null!;
     }
 }
