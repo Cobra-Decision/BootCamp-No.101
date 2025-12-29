db75be1dfb0536f93c98ee4edd184d77b0927cb5
negin <negin1380farahani@gmail.com>
2025-12-29 13:21:19 +0330

    feat(ITicketStatusService): add interface for ticketstatus

---
+++ Ticketing.Application/Interfaces/Services/ITicketStatusService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticketing.Application.DTO;
+
+
+namespace Ticketing.Application.Interfaces.Services
+{
+    public interface ITicketStatusService
+    {
+
+        public Task Remove(int id);
+        public Task<List<StatusDto>> GetAll();
+        public Task Edit(int ticketid, StatusDto dto);
+
+
+
+
+    }
+}
