f5424c2236e676ea98c0b79eea72a3d7af3ef578
negin <negin1380farahani@gmail.com>
2025-12-29 17:12:16 +0330

    feat(TicketStatusController): add TicketStatusController for ticket status management

---
+++ Ticketing.Presentation/Controllers/TicketStatusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ticketing.Application.DTO;
+using Ticketing.Application.Interfaces.Services;
+
+namespace Ticketing.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketStatusController : ControllerBase
+    {
+        private readonly ITicketStatusService _statusService;
+        public TicketStatusController(ITicketStatusService statusService)
+        {
+            _statusService = statusService;
+        }
+
+
