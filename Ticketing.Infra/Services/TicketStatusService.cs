a47d85ab1218aa25426cb1ddcea3ce60adf91b8a
negin <negin1380farahani@gmail.com>
2025-12-29 16:09:45 +0330

    feat(infra/TicketStatusService ): implement TicketStatusService for managing ticket statuses

---
+++ Ticketing.Infra/Services/TicketStatusService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticketing.Application.DTO;
+using Ticketing.Application.Interfaces.Services;
+using Ticketing.Domain;
+
+namespace Ticketing.Infra.Services
+{
+    public  class TicketStatusService : ITicketStatusService
+    {
+        private readonly DatabaseContext _db;
+        public TicketStatusService(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+
+
