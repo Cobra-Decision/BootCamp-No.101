55febad8bd7564f620bd5d512bcbbbe01ac32706
negin <negin1380farahani@gmail.com>
2025-12-29 15:26:03 +0330

    feat(Presentation/UserTicketController ): add UserTicketController for user ticket management

---
+++ Ticketing.Presentation/Controllers/UserTicketController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using Ticketing.Infra.Services;
+
+namespace Ticketing.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTicketController : ControllerBase
+    {
+        private readonly UserTicketService _userTicketService;
+        public UserTicketController (UserTicketService userTicketService)
+        {
+            _userTicketService = userTicketService;
+        }
+
+
