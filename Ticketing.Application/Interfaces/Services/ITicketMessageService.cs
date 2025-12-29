a5805b9d1cc2ee1b03631c40bedcae04f424c298
negin <negin1380farahani@gmail.com>
2025-12-29 13:18:12 +0330

    feat(Application/iticketmessageService): add interface for create ticketmessageservice

---
+++ Ticketing.Application/Interfaces/Services/ITicketMessageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticketing.Application.DTO;
+
+namespace Ticketing.Application.Interfaces.Services
+{
+    public interface ITicketMessageService
+    {
+         Task<TicketMessageDto> AddMessage(int ticketid, TicketMessageDto dto);
+         Task<List<TicketMessageDto>> GetMessage(int ticketid);
+
+    }
+}
